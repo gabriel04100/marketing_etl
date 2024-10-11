890683dc59b023beb00116fadefa70a3862e1a4c
gabriel04100 <gabrielp@outlook.fr>
2024-10-11 22:54:37 +0200

    flattened json

--- etl/extract.py
+++ etl/extract.py
@@ -14,18 +14,21 @@ logging.basicConfig(
 
 MONGO_URL = os.getenv("MONGODB_URI")
 
-def extract_collection(mongo_url, db, collection, filter=None, projection=None):
-        try:
-            with MongoClient(mongo_url) as client:
-                db=client[db]
-                collection=db[collection]
-                cursor = collection.find(filter, projection)
-                logging.info("extract sucessfull")
-                return list(cursor)
-        except errors.PyMongoError as e:
-            logging.error(f"extract failed {e}")
-            return {}
-        
-db="product_transactions"
-collection="users"
-print(extract_collection(MONGO_URL,db,collection))
+
+def extract_collection(mongo_url, db,
+                       collection, filter=None, projection=None):
+    try:
+        with MongoClient(mongo_url) as client:
+            db = client[db]
+            collection = db[collection]
+            cursor = collection.find(filter, projection)
+            logging.info("extract sucessfull")
+            return list(cursor)
+    except errors.PyMongoError as e:
+        logging.error(f"extract failed {e}")
+        return {}
+
+
+db = "product_transactions"
+collection = "users"
+print(extract_collection(MONGO_URL, db, collection))
